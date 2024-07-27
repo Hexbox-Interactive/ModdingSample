9fdae3b5f68ccf90b56bd5e6c07da3c4a6626c51
Nathan Schmitt <47279783+IBXCODECAT@users.noreply.github.com>
2024-07-27 00:31:13 -0500

    Add Example Mod

---
+++ Example/ExampleMod/ExampleMod.cs
@@ -0,0 +1,108 @@
+﻿// Example mod definition by Nathan Schmitt
+
+// NOTE: You must include the game's assembly in your project to reference the game's code.
+// The IBX.Mods namespace contains all the classes and interfaces required to create a mod.
+// More information about the IBX.Mods namespace can be found in the documentation.
+using IBX.Mods;
+
+// You can add more using statements if you wish, just keep in mind that the game's mod loader
+// will refuse to load a mod that is referencing non-whitelisted assemblies.
+//
+// Here is a list of white-listed assemblies your project can reference:
+// - mscorelib
+// - System
+// - System.Core
+// - System.Runtime
+// - GodotSharp
+// - The game's main assembly [REQUIRED] (contains the game's code and mod loader)
+//
+// If there is a specific assembly you would like to reference that is not on this list, please
+// create an issue on this repository or contact the developers on the official Discord server.
+//
+// In the meantime, you can copy the code from the assembly you are trying to reference and use it
+// in your mod directly. This is not ideal, but it is a workaround until we can add more assemblies
+// to the white-list.
+
+// You can use whatever namespace you want, but it is recommended to use the name of your mod.
+namespace ExampleMod
+{
+    /// <summary>
+    /// All mods are required to have a mod definition. This is the main file for our mod.
+    /// There should only ever be one mod definition per mod. Multiple mod definitions
+    /// will cause unpredictable and undefined behavior. Mod definitions are loaded automatically 
+    /// by the game's internal mod loader at startup. All other mod components are managed
+    /// only when the mod is enabled.
+    /// 
+    /// Mods must implement the <see cref="IUserModDefinition"/> interface which contains the mod's
+    /// metadata as well as a <see cref="SharedModInstance"/>.
+    /// </summary>
+    internal class ExampleMod : IUserModDefinition
+    {
+        /// <summary>
+        /// You are required to create a new instance of the <see cref="ModInstance"/> class.
+        /// </summary>
+        /// 
+        /// <remarks>
+        /// Inside the game's code, the <see cref="ModInstance"/> class defines two fields: a 
+        /// calling assembly and a reference to an internal "mod object."
+        /// 
+        /// These are used by the game to manage mods, execute their scripts, and facilitate and 
+        /// validate extensions. The mod loader will handle the loading and initialization of your 
+        /// mod to a certain degree using the mod's manifest file, but it is not smart enough to 
+        /// automatically determine the mod's assembly. Under the hood, when you construct a 
+        /// <see cref="ModInstance"/>(), you are telling the game what the mod's "Calling Assembly" 
+        /// is as well as associating it with the appropriate internal mod object. This process 
+        /// ensures your the mod is correctly identified, validated, and integrated into the game.
+        /// 
+        /// Additionally, by manually creating the ModInstance, you maintain control over the mod’s lifecycle 
+        /// and interactions. This approach allows you to request operations from the mod loader directly,
+        /// ensuring that only properly initialized and enabled mods can make requests.
+        /// It also helps enforce validation and control mechanisms, improving the stability and security 
+        /// of the modding system.
+        /// </remarks>
+        public static readonly ModInstance Instance = new();
+
+        /// <summary>
+        /// This property is required by the <see cref="IUserModDefinition"/> interface to
+        /// provide the <see cref="ModInstance"/> to the game and mod loader.
+        /// </summary>
+        public ModInstance SharedModInstance { get { return Instance; } }
+
+        /// <summary>
+        /// The name of the mod.
+        /// </summary>
+        /// <remarks>
+        /// This is different from the value defined in the mod's manifest file.
+        /// This will only be displayed in the debugger and log files.
+        /// </remarks>
+        public string Name => "Example Mod";
+
+        /// <summary>
+        /// The version of the mod.
+        /// </summary>
+        /// <remarks>
+        /// This is different from the value defined in the mod's manifest file.
+        /// This will only be displayed in the debugger and log files.
+        /// </remarks>
+        public string Description => "An example mod for documentation.";
+
+        /// <summary>
+        /// The author of the mod.
+        /// </summary>
+        /// <remarks>
+        /// This is different from the value defined in the mod's manifest file.
+        /// This will only be displayed in the debugger and log files.
+        /// </remarks>
+        public string Author => "Nathan Schmitt";
+
+        /// <summary>
+        /// The copyright notice of the mod.
+        /// You can use this to specify the year the mod was created and whom it belongs to.
+        /// </summary>
+        /// <remarks>
+        /// This is different from the value defined in the mod's manifest file.
+        /// This will only be displayed in the debugger and log files.
+        /// </remarks>
+        public string Copyright => "Copyright (C) 2024 Nathan Schmitt";
+    }
+}
---
+++ Example/ExampleMod/Scripts/SampleScript.cs
@@ -0,0 +1,74 @@
+﻿// Example mod script by Nathan Schmitt
+
+// This script is an example of a mod script that can be used to add functionality to the game.
+// This script will print messages to the console when the mod is enabled, disabled, and once every X frames.
+
+using Godot;
+using IBX.Mods;
+
+// You can use whatever namespace you want, but it is recommended to use the name of your mod.
+namespace ExampleMod
+{
+    /// <summary>
+    /// Mod scripts are used to add functionality to the game without overriding any 
+    /// already existing functionality. If you would like to override existing 
+    /// functionality, you should create a new class that inherits from the various 
+    /// "UserModExtension" classes you would like to override.
+    /// 
+    /// 1. Mod scripts are loaded automatically by the game's internal mod loader when the mod is enabled.
+    /// 2. Mod scripts are executed one at a time in the order that they were loaded.
+    /// 3. There is no way to guarantee the order in which mod scripts are loaded for performance reasons.
+    /// 4. It is the responsibility of the mod developer to keep track of the order in which mod scripts are loaded.
+    /// </summary>
+    public sealed class SampleScript : IUserModScript
+    {
+        /// <summary>
+        /// This method is called by the mod loader when the mod is enabled.
+        /// </summary>
+        /// <remarks>
+        /// Use this method to initialize your mod and set up any necessary resources.
+        /// </remarks>
+        public void OnEnable()
+        {
+            GD.Print("Hello from mod script 1!");
+
+            ExampleMod.Instance.RequestDisable();
+        }
+
+        /// <summary>
+        /// This method is called by the mod loader when the mod is disabled.
+        /// </summary>
+        /// <remarks>
+        /// Use this method to clean up any resources and save any necessary data.
+        /// </remarks>
+        public void OnDisable()
+        {
+            GD.Print("Goodbye from mod script 1!");
+            ExampleMod.Instance.RequestReload();
+        }
+
+        /// <summary>
+        /// This method is called by the mod loader every frame
+        /// </summary>
+        /// <param name="delta">The delta time between frames</param>
+        /// <remarks>
+        /// Use this method to update your mod's logic every frame if necessary.
+        /// </remarks>
+        public void OnProcess(double delta)
+        {
+            GD.Print("MOD PROCESSING 1!");
+        }
+
+        /// <summary>
+        /// This method is called by the mod loader every physics update
+        /// </summary>
+        /// <param name="delta">The delta time between updates</param>
+        /// <remarks>
+        /// Use this method to update your mod's physics logic every physics update if necessary.
+        /// </remarks>
+        public void OnPhysicsProcess(double delta)
+        {
+            GD.Print("MOD PHYSICS PROCESSING 1!");
+        }
+    }
+}
